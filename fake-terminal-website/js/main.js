3fa727cdbd229cd49e522c09378dd097c6e9b0d8
DavidPaper <david.sosa@paperdice.de>
2021-11-03 14:16:29 +0100

    removed not needed commands

--- fake-terminal-website/js/main.js
+++ fake-terminal-website/js/main.js
@@ -13,36 +13,14 @@ var configs = (function () {
   };
   Singleton.defaultOptions = {
     bruteForceAttack: "0x00 - 0xff Progression: [1-99%] Estimated process duration ~ [3: 30min]",
-    general_help: "Below there's a list of commands that you can use.\nYou can use autofill by pressing the TAB key, autocompleting if there's only 1 possibility, or showing you a list of possibilities.",
-    ls_help: "List information about the files and folders (the current directory by default).",
-    cat_help: "Read FILE(s) content and print it to the standard output (screen).",
-    whoami_help: "Print the user name associated with the current effective user ID and more info.",
-    date_help: "Print the system date and time.",
-    help_help: "Print this menu.",
-    clear_help: "Clear the terminal screen.",
-    reboot_help: "Reboot the system.",
-    cd_help: "Change the current working directory.",
-    mv_help: "Move (rename) files.",
-    rm_help: "Remove files or directories.",
-    rmdir_help: "Remove directory, this command will only work if the folders are empty.",
-    touch_help: "Change file timestamps. If the file doesn't exist, it's created an empty one.",
-    sudo_help: "Execute a command as the superuser.",
     welcome: "This is the welcome message",
     internet_explorer_warning: "NOTE: I see you're using internet explorer, this website won't work properly.",
-    welcome_file_name: "welcome_message.txt",
     invalid_command_message: "<value>: command not found.",
-    reboot_message: "Preparing to reboot...\n\n3...\n\n2...\n\n1...\n\nRebooting...\n\n",
-    permission_denied_message: "Unable to '<value>', permission denied.",
     sudo_message: "Unable to sudo using a web client.",
     usage: "Usage",
     file: "file",
     file_not_found: "File '<value>' not found.",
-    username: "Username",
-    hostname: "Host",
-    platform: "Platform",
-    accesible_cores: "Accessible cores",
     language: "Language",
-    value_token: "<value>",
     host: "example.com",
     user: "guest",
     is_root: false,
@@ -310,7 +288,7 @@ var main = (function () {
   };
 
   Terminal.prototype.handleCmd = function () {
-    var cmdComponents = this.cmdLine.value.trim().toLowerCase();;
+    var cmdComponents = this.cmdLine.value.trim().toLowerCase();
     this.lock();
     switch (cmdComponents) {
       case cmds.BFA.value:
