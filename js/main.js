347888aa3986560415bf3e78d0a7fcffbdfebede
DavidPaper <david.sosa@paperdice.de>
2021-11-04 16:17:24 +0100

    right time

--- js/main.js
+++ js/main.js
@@ -205,7 +205,7 @@ var main = (function () {
 
       this.type(bruteText);
       if(percentage < 10){
-        await sleep(2100);
+        await sleep(21000);
       } else {
         await sleep(1400);
       }
