2179efe465b1a45fdd235069cf4097c0c736dd9f
DavidPaper <david.sosa@paperdice.de>
2021-11-04 14:12:02 +0100

    shorter time for evaluation

--- fake-terminal-website/js/main.js
+++ fake-terminal-website/js/main.js
@@ -204,7 +204,7 @@ var main = (function () {
       console.log(bruteText);
 
       this.type(bruteText);
-      await sleep(21000);
+      await sleep(2100);
       percentage++;
     }
     this.type("Brute force attack completed");
